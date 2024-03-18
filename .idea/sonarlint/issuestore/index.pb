1733469e748c66d704ed0542323f387b79025e11
winkywong1998 <winkywong1998@gmail.com>
2024-03-17 22:28:13 -0400

    Fix the add group

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@
 WFIXmsgGenerator/src/main/java/com/winky/fixmsggenerator/FiXmsgGeneratorApplication.java,c\a\ca0534711bb9df93e78ccc2deda6ff70ccec0dba
 …
 UFIXmsgGenerator/src/main/java/com/winky/fixmsggenerator/util/FIXmsgToQuickFIXObj.java,8\a\8ab2f3ffcd88b7745c4c9fea0b730133d485b5b3
+Š
+ZFIXmsgGenerator/src/main/java/com/winky/fixmsggenerator/util/FIXmsgToExecutionReport3.java,7\0\7046a725927dde99ca2ae1d660a507ad6a78695a
+G
+FIXmsgGenerator/pom.xml,f\6\f63192ee26a3f0fb652d58ff564b1d2b4137e22d
--- FIXmsgGenerator/src/main/java/com/winky/fixmsggenerator/util/JSONtoFIXmsg2.java
+++ FIXmsgGenerator/src/main/java/com/winky/fixmsggenerator/util/JSONtoFIXmsg2.java
@@ -26,7 +26,8 @@ public class JSONtoFIXmsg2 {
 
     public static String generateFIXMessage(JSONObject input) {
         Message message = new Message();
-        message.getHeader().setString(MsgType.FIELD, "8"); // Message Type
+        // Set message type. '8' is actually the MsgType for ExecutionReport in FIX 4.4, update as necessary
+        message.getHeader().setString(MsgType.FIELD, "8");
         for (String key : input.keySet()) {
             int field = getFieldNumber(key);
             if (field > 0) {
@@ -34,106 +35,55 @@ public class JSONtoFIXmsg2 {
                 if (value instanceof String) {
                     message.setString(field, (String) value);
                 } else if (value instanceof Integer) {
-                    message.setInt(field, (int) value);
+                    message.setInt(field, (Integer) value);
                 } else if (value instanceof Double) {
-                    message.setDouble(field, (double) value);
-                } else if (value instanceof JSONArray) {
-                    int fieldTag = getFieldNumber(key);
-                    multipleValTag.add(fieldTag);
-                    JSONArray jsonArray = (JSONArray) value;
-                    Group group = null;
-                    for (int i = 0; i < jsonArray.length(); i++) {
-                        Object fieldValue = jsonArray.get(i);
-                        if (group == null) {
-                            int delimiterField = getFieldNumber(key) - 1;
-                            group = new Group(field, delimiterField);
-                        }
-                        if (fieldValue instanceof String) {
-                            group.setString(fieldTag, (String) fieldValue);
-                        } else if (fieldValue instanceof Integer) {
-                            group.setInt(fieldTag, (Integer) fieldValue);
-                        } else if (fieldValue instanceof Double) {
-                            group.setDouble(fieldTag, (Double) fieldValue);
-                        }
-                        message.addGroup(group);
-                    }
-
+                    message.setDouble(field, (Double) value);
                 }
             }
         }
-        String fixMessage = message.toString();
-        // Replace SOH with |
-        fixMessage = fixMessage.replaceAll("\u0001", "|");
-        if(!multipleValTag.isEmpty()) fixMessage = removeFirstOccurrences(fixMessage, multipleValTag);
-        fixMessage = sortTags(fixMessage);
+        // Handling NoSecurityAltID group
+        if (input.has("NoSecurityAltID")) {
+            handleSecurityAltIDGroup(input, message);
+        }
+
+        // Handling NoMiscFees group
+        if (input.has("NoMiscFees")) {
+            handleMiscFeesGroup(input, message);
+        }
+
+        String fixMessage = message.toString().replaceAll("\u0001", "|");
         return fixMessage;
+    }
+
+    private static void handleSecurityAltIDGroup(JSONObject input, Message message) {
+        JSONArray altIds = input.getJSONArray("SecurityAltID");
+        JSONArray altIdSources = input.getJSONArray("SecurityAltIDSource");
+        Group securityAltIdGroup = new Group(454, 455);
+
+        for (int i = 0; i < altIds.length(); i++) {
+            securityAltIdGroup.setString(455, altIds.getString(i));
+            securityAltIdGroup.setString(456, altIdSources.getString(i));
+            message.addGroup(securityAltIdGroup);
+        }
+    }
+
+    private static void handleMiscFeesGroup(JSONObject input, Message message) {
+        JSONArray feeAmts = input.getJSONArray("MiscFeeAmt");
+        JSONArray feeCurrs = input.getJSONArray("MiscFeeCurr");
+        JSONArray feeTypes = input.getJSONArray("MiscFeeType");
+        JSONArray feeBases = input.getJSONArray("MiscFeeBasis");
+        Group miscFeesGroup = new Group(136, 137);
+
+        for (int i = 0; i < feeAmts.length(); i++) {
+            miscFeesGroup.setDouble(137, feeAmts.getDouble(i));
+            miscFeesGroup.setString(138, feeCurrs.getString(i));
+            miscFeesGroup.setString(139, feeTypes.getString(i));
+            miscFeesGroup.setString(891, feeBases.getString(i));
+            message.addGroup(miscFeesGroup);
+        }
     }
 
     private static int getFieldNumber(String fieldName) {
         return dataDictionary.getFieldTag(fieldName);
-    }
-
-    public static String removeFirstOccurrences(String fixMessage, List<Integer> fieldsToRemove) {
-        String[] parts = fixMessage.split("\\|");
-        StringBuilder result = new StringBuilder();
-        for (int field : fieldsToRemove) {
-            boolean removed = false;
-            for (int i = 0; i < parts.length; i++) {
-                if (!removed && parts[i].startsWith(field + "=")) {
-                    // If the field matches and hasn't been removed yet, skip this part
-                    removed = true;
-                    continue;
-                }
-                // Add the part back to the result, including the delimiter if it's not the first part
-                if (result.length() > 0) {
-                    result.append("|");
-                }
-                result.append(parts[i]);
-            }
-            // Replace the original parts with the newly constructed parts for subsequent removals
-            parts = result.toString().split("\\|");
-            // Reset the StringBuilder for the next iteration
-            result = new StringBuilder();
-        }
-
-        // Construct the final result from the remaining parts
-        for (String part : parts) {
-            if (result.length() > 0) {
-                result.append("|");
-            }
-            result.append(part);
-        }
-        return result.toString();
-    }
-
-    public static String sortTags(String input) {
-        String[] parts = input.split("\\|");
-        List<String> tagValuePairs = Arrays.asList(parts);
-
-        // Custom comparator to sort tag-value pairs based on tag number
-        Comparator<String> comparator = new Comparator<String>() {
-            @Override
-            public int compare(String pair1, String pair2) {
-                int tag1 = Integer.parseInt(pair1.split("=")[0]);
-                int tag2 = Integer.parseInt(pair2.split("=")[0]);
-                return Integer.compare(tag1, tag2);
-            }
-        };
-
-        // Sort the tag-value pairs
-        Collections.sort(tagValuePairs, comparator);
-
-        // Concatenate sorted tag-value pairs back into a single string
-        StringBuilder sortedString = new StringBuilder();
-        for (String pair : tagValuePairs) {
-            sortedString.append(pair).append("|");
-        }
-
-        // Remove trailing '|'
-        if (sortedString.length() > 0) {
-            sortedString.deleteCharAt(sortedString.length() - 1);
-        }
-
-        return sortedString.toString();
     }
 }
